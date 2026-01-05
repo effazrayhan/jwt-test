e18ac32dddb643b242c5e668ac2ec592a2bea56f
Effaz Rayhan Anando <143859628+effazrayhan@users.noreply.github.com>
2026-01-05 18:42:47 +0600

    First Commit

---
+++ authMiddleware.js
@@ -0,0 +1,24 @@
+const jwt = require("jsonwebtoken");
+
+const SECRET_KEY = "delhinadhaka";
+
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).json({ message: "No token provided" });
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  jwt.verify(token, SECRET_KEY, (err, decoded) => {
+    if (err) {
+      return res.status(403).json({ message: "Invalid token" });
+    }
+
+    req.user = decoded;
+    next();
+  });
+}
+
+module.exports = authenticateToken;
